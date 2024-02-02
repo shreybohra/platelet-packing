cf8c815a07abb1aee86cc97a558bbd3a2f609f3a
Shrey Bohra <42218014+shreybohra@users.noreply.github.com>
2024-02-02 00:15:48 +0000

    fix rect class

--- circles.py
+++ circles.py
@@ -4,6 +4,7 @@ import pygame
 import pymunk
 import pymunk.pygame_util
 import random
+import math
 
 # initial parameters
 dt = 0.01
@@ -40,21 +41,40 @@ floor = pymunk.Segment(space.static_body, (0, 5), (box_width, 5), 5)
 left_wall = pymunk.Segment(space.static_body, (0, 5), (0, box_height), 5)
 right_wall = pymunk.Segment(space.static_body, (box_width, 5), (box_width, box_height), 5)
 
+floor.elasticity = 0.95
+floor.friction = 0.5
+
+left_wall.elasticity = 0.95
+left_wall.friction = 0.5
+
+right_wall.elasticity = 0.95
+right_wall.friction = 0.5
+
 space.add(floor, left_wall, right_wall)
 
 #%%
 
 # create a class to generate rectangles
 class Rectangle:
-    def __init__(self, box_width, box_height):
+    def __init__(self, box_width, box_height, width_gen, ar_gen):
         self.box_width = box_width
         self.box_height = box_height
+        self.width_gen = width_gen
+        self.ar_gen = ar_gen
+        self.density = 1       
         
-        
+        self.y = box_height + 100
+        self.x = random.uniform(0, self.box_width - width)  # Ensure it fits within the box
 
-    def create(self, x, y, width, height, mass):
+    def create(self):
+        width = self.width_gen.generate()
+        height = width * self.ar_gen.generate()
+        mass = width * height * self.density
+        angle = random.uniform(-0.5*math.pi, 0.5*math.pi)
+
         self.body = pymunk.Body(mass, pymunk.moment_for_box(mass, width, height))
-        self.body.position = x, y
+        self.body.position = self.x, self.y
+        self.body.angle = angle
         self.shape = pymunk.Poly.create_box(self.body, (width, height))
         self.shape.elasticity = 0.95
         self.shape.friction = 0.9
@@ -63,7 +83,31 @@ class Rectangle:
     def draw(self):
         pygame.draw.polygon(screen, (0, 0, 0), self.shape.get_vertices())
 
-# create a class for normal distribution of dimensions
+# game loop
         
+running = True
+clock = pygame.time.Clock()
 
+platelets = Rectangle(box_width, box_height)
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            
+    screen.fill((255, 255, 255))
+    space.debug_draw(draw_options)
+    pygame.display.flip()
+    space.step(dt)
+    clock.tick(60)
+    
+    if random.random() < 0.1:
+        width = width_gen.generate()
+        height = width / ar_gen.generate()
+        mass = 1
+        x = random.randint(0, box_width)
+        y = box_height
+        rect = Rectangle(box_width, box_height)
+        rect.create(x, y, width, height, mass)
+        rect.draw()
         
