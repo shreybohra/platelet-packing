f4fd1a536b34fb0cfaa9029cb6f04d65b4234804
Shrey Bohra <42218014+shreybohra@users.noreply.github.com>
2024-02-23 00:31:23 +0000

    width/height generators

--- pybullet/blocks.py
+++ pybullet/blocks.py
@@ -8,3 +8,16 @@ import math
 import random
 
 #%%
+class Distribution:
+    def __init__(self, mean, std):
+        self.mean = mean
+        self.std = std
+        
+    def generate(self):
+        return random.gauss(self.mean, self.std)
+
+width_gen = Distribution(width_mean, width_std)
+ar_gen = Distribution(ar_mean, ar_std)
+depth = 5
+
+#%%
