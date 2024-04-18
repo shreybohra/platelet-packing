d99d4297303b8756be267eadd0aaf696724833f4
Shrey Bohra <42218014+shreybohra@users.noreply.github.com>
2024-04-19 00:42:16 +0100

    autostop for spheres

--- pybullet/spheres.py
+++ pybullet/spheres.py
@@ -33,7 +33,7 @@ container_body_id = p.createMultiBody(0, container_id, container_visual_id, [0, 
 
 # Create collision shapes for the container walls
 wall_thickness = 0.1
-wall_height = 5  
+wall_height = 2  
 half_extents_x = (container_size[0] + wall_thickness)
 half_extents_y = (container_size[1] + wall_thickness)
 
@@ -57,10 +57,10 @@ p.createMultiBody(0, bottom_wall_id, basePosition=[0, -container_size[1] - wall_
 
 #%%
 def generate_random_position(container_size=container_size):
-    x = random.uniform(-container_size[0]+1, container_size[0]-1)
-    y = random.uniform(-container_size[1]+1, container_size[1]-1)
+    x = random.uniform(-container_size[0]+2, container_size[0]-2)
+    y = random.uniform(-container_size[1]+2, container_size[1]-2)
     z = container_size[2] + 20
-    orientation = p.getQuaternionFromEuler([random.uniform(0, 2 * 3.1416) for _ in range(3)])
+    orientation = p.getQuaternionFromEuler([random.uniform(0, 2 * math.pi) for _ in range(3)])
     return [x, y, z], orientation
 
 def create_sphere(radius, position, orientation):
@@ -77,6 +77,7 @@ def check_collision(sphere_id, existing_spheres):
     for sphere in existing_spheres:
         contact_points = p.getClosestPoints(sphere_id, sphere, distance=0.5)
         if contact_points:
+            print("Collision detected")
             return True # collision detected
     return False
 
@@ -110,6 +111,7 @@ def check_overflow(settled_spheres, container_size):
     for sphere in settled_spheres:
         pos, _ = p.getBasePositionAndOrientation(sphere)
         if pos[2] > container_size[2]:
+            print ("overflow detected")
             return True
     return False
 
@@ -118,46 +120,83 @@ settled_spheres = []
 overflow = False
 
 radius = 1
-total_vol = 0
+generated_vol = 0
+container_vol = container_size[0] * container_size[1] * wall_height * 4 # half extents
 
 print("Creating spheres...")
-while not overflow:
+# while not overflow:
+while generated_vol < container_vol*2: #allow for some extra
     pos, orn = generate_random_position()
     
+    print(f"Generating sphere {len(existing_spheres)+1}")
     platelet_id = create_sphere(radius, pos, orn)
-    total_vol += (4/3) * math.pi * radius**3
+    generated_vol += (4/3) * math.pi * radius**3
 
     while check_collision(platelet_id, existing_spheres):
+        print("Setting new position")
         pos, orn = generate_random_position()
         p.resetBasePositionAndOrientation(platelet_id, pos, orn)
 
-    for _ in range(20):
+    for _ in range(50):
         p.stepSimulation()
         time.sleep(dt)
         
     existing_spheres.append(platelet_id)
     
-    for sphere in existing_spheres:
-        if sphere not in settled_spheres:
-            print(f"Checking sphere {sphere}")
-            if check_settled(sphere, settled_spheres):
-                pos, _ = p.getBasePositionAndOrientation(sphere)
-                if pos[2] <= container_size[2]:
-                    settled_spheres.append(sphere)
+    # for sphere in existing_spheres:
+    #     if sphere not in settled_spheres:
+    #         print(f"Checking sphere {sphere}")
+    #         if check_settled(sphere, settled_spheres):
+    #             pos, _ = p.getBasePositionAndOrientation(sphere)
+    #             if pos[2] <= container_size[2]:
+    #                 settled_spheres.append(sphere)
 
-    print(f"Number of settled spheres: {len(settled_spheres)}")
+    # print(f"Number of settled spheres: {len(settled_spheres)}")
     overflow = check_overflow(settled_spheres, container_size)
 
-    if len(existing_spheres) > 300:
-        break
+    # if len(existing_spheres) > 200:
+    #     break
 
 print("Settling...")
+settle_time = 0
 while check_movement(existing_spheres):
     for _ in range(10):
         p.stepSimulation()
         time.sleep(dt)
+        settle_time += dt
+    if settle_time > 20:
+        break
 
-print(f"Total volume of spheres: {total_vol} m^3")
-print(f"Volume fraction: {total_vol/(container_size[0]*container_size[1]*container_size[2])}")
+print("Settled")
+#%%
+## isolate the spheres which lie within the container
+
+def get_sphere_properties(sphere_id):
+    pos, orn = p.getBasePositionAndOrientation(sphere_id)
+    radius = p.getVisualShapeData(sphere_id)[0][3][0]
+    return pos, radius
+
+def calculate_sphere_volume(radius):
+    return (4/3) * math.pi * radius**3
+
+def is_inside_container(pos, radius, container_size):
+    x_check = abs(pos[0]) <= container_size[0]
+    y_check = abs(pos[1]) <= container_size[1]
+    z_check = pos[2] - radius <= container_size[2]
+
+    return (x_check and y_check and z_check)
+
+inside_vol = 0
+for sphere in existing_spheres:
+    # change color to denote it's been filtered
+    pos, radius = get_sphere_properties(sphere)
+    if is_inside_container(pos, radius, container_size):
+        p.changeVisualShape(sphere, -1, rgbaColor=[0, 0, 0, 1])
+        inside_vol += calculate_sphere_volume(radius)
+#%%
+print(f"Total volume of spheres: {generated_vol} m^3")
+print(f"Volume of spheres inside container: {inside_vol} m^3")
+print(f"Volume fraction: {inside_vol/(container_vol)}")
 
 simend = input("Continue?")
+p.disconnect()
