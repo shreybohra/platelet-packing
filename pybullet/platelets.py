32d01ff506b3d1ed4472d5ecde55bebdbac4e7f9
Shrey Bohra <42218014+shreybohra@users.noreply.github.com>
2024-04-24 21:20:45 +0100

    switch to subclass structurte

--- pybullet/platelets.py
+++ pybullet/platelets.py
@@ -10,8 +10,7 @@ import random
 
 class Platelet:
 
-    def __init__(self, type, density = 1, friction = 0.5, restitution = 0.2, dims=[]):
-        self.type = type
+    def __init__(self, density = 1, friction = 0.5, restitution = 0.2):
         self.density = density
         self.friction = friction
         self.restitution = restitution
@@ -27,11 +26,23 @@ class Platelet:
     
 
 
-    class Sphere:
+class Sphere(Platelet):
 
-        def __init__(self, radius, mass):
-            self.radius = radius
-            self.mass = mass
+    def __init__(self, radius, density = 1, friction = 0.5, restitution = 0.2, radius_generator = None):
+        super().__init__(density, friction, restitution)
+        self.radius = radius
+
+    def set_radius(self, radius):
+        self.radius = radius
+
+    def get_volume(self):
+        return (4/3) * math.pi * self.radius**3
+
+    def get_mass(self):
+        return self.density * self.get_volume()
+
+    def get_inertia(self):
+        return (2/5) * self.get_mass() * self.radius**2
 
 
             
