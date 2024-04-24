91de947da47ddaa6d3edaa07695938372ca59e2b
Shrey Bohra <42218014+shreybohra@users.noreply.github.com>
2024-04-24 21:19:09 +0100

    create plateltes using nested class structure

---
+++ pybullet/platelets.py
@@ -0,0 +1,37 @@
+import pybullet as p
+import pybullet_data
+import time
+import numpy as np
+import math
+import random
+
+
+#%%
+
+class Platelet:
+
+    def __init__(self, type, density = 1, friction = 0.5, restitution = 0.2, dims=[]):
+        self.type = type
+        self.density = density
+        self.friction = friction
+        self.restitution = restitution
+
+    def set_density(self, density):
+        self.density = density
+
+    def set_friction(self, friction):
+        self.friction = friction
+    
+    def set_restitution(self, restitution):
+        self.restitution = restitution
+    
+
+
+    class Sphere:
+
+        def __init__(self, radius, mass):
+            self.radius = radius
+            self.mass = mass
+
+
+            
